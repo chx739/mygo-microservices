96bf89b063c027ff67ab8b65eaf5db68498dbb43
chx739 <1377954361@qq.com>
2026-03-19 16:10:00 +0800

    优化API网关的服务器启动和关闭逻辑，增加优雅关闭支持

--- services/trip-service/cmd/main.go
+++ services/trip-service/cmd/main.go
@@ -8,6 +8,22 @@ import (
 	"ride-sharing/services/trip-service/internal/service"
 )
 
+/*
+curl -X POST http://127.0.0.1:8081/trip/preview \
+  -H "Content-Type: application/json" \
+  -d '{
+    "userID": "12",
+    "destination": {
+      "latitude": 37.7960377140795,
+      "longitude": -122.42859007644311
+    },
+    "pickup": {
+      "latitude": 37.783678087129466,
+      "longitude": -122.40777303207217
+    }
+  }'
+*/
+
 func main() {
 	inmemRepo := repository.NewInmemRepository()
 	svc := service.NewService(inmemRepo)
